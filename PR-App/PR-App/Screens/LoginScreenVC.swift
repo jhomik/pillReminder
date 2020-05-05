d2573c830379a8c8e2b7b55dc959c5ac02e2e5ef
Jakub <jh.homik@gmail.com>
2020-05-05 16:05:42 +0200

    Delete Storyboard, added rootViewController

---
+++ PR-App/PR-App/Screens/LoginScreenVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginScreenVC.swift
+//  PR-App
+//
+//  Created by Jakub Homik on 05/05/2020.
+//  Copyright © 2020 Jakub Homik. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
