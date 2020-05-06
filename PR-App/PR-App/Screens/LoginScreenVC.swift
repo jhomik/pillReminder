3a633e35900a29fbf5b7a271ccda8774165d3920
Jakub <jh.homik@gmail.com>
2020-05-06 17:26:09 +0200

    Created WelcomeScreen UIView in "LoginScreenVC"

--- PR-App/PR-App/Screens/LoginScreenVC.swift
+++ PR-App/PR-App/Screens/LoginScreenVC.swift
@@ -9,22 +9,54 @@
 import UIKit
 
 class LoginScreenVC: UIViewController {
-
+    
+    let welcomeView = UIView()
+    let welcomeImage = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureWelcomeView()
+        performAnimations()
+        
+        view.backgroundColor = .systemPink
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureWelcomeView() {
+        view.addSubview(welcomeView)
+        welcomeView.frame = view.bounds
+        welcomeView.backgroundColor = .systemBackground
+        
+        welcomeView.addSubview(welcomeImage)
+        welcomeImage.translatesAutoresizingMaskIntoConstraints = false
+        welcomeImage.image = UIImage(named: "logo-PR")
+        welcomeImage.frame = CGRect(x: 0, y: -20, width: 200, height: 180)
+        
+        NSLayoutConstraint.activate([
+            welcomeImage.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor),
+            welcomeImage.centerYAnchor.constraint(equalTo: welcomeView.centerYAnchor),
+            welcomeImage.widthAnchor.constraint(equalToConstant: 200),
+            welcomeImage.heightAnchor.constraint(equalToConstant: 180)
+            
+        ])
     }
-    */
-
+    
+    private func performAnimations() {
+        
+        welcomeImage.alpha = 0.0
+        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseOut, animations: {
+            self.welcomeImage.frame = CGRect(x: 0, y: -600, width: 200, height: 180)
+            self.welcomeImage.alpha = 1.0
+        }) { (finished) in
+            UIView.animate(withDuration: 2, delay: 1, options: .curveEaseOut, animations: {
+                self.welcomeImage.alpha = 0.0
+            }) { (finished) in
+                UIView.transition(with: self.welcomeView, duration: 1, options: .transitionCrossDissolve, animations: {
+                    self.welcomeView.alpha = 0.0
+                }, completion: nil)
+            }
+        }
+        
+        
+        
+    }
 }
