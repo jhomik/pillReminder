994ed1881c5db261b1b89dd58c344c51b48c2b05
Jakub <jh.homik@gmail.com>
2020-05-08 18:47:02 +0200

    delete PODS folder

--- PR-App/Pods/Target Support Files/Pods-PR-App-PR-AppUITests/Pods-PR-App-PR-AppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PR_App_PR_AppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PR_App_PR_AppUITests
-@end
--- PR-App/Pods/Target Support Files/Pods-PR-AppTests/Pods-PR-AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PR_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_PR_AppTests
-@end
