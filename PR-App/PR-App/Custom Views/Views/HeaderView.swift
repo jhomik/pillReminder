a39bd8d05a3880ecb89323d495c4dc09b0148c48
Jakub <jh.homik@gmail.com>
2020-07-27 17:48:49 +0200

    added TableView to NewMedicationVC

---
+++ PR-App/PR-App/Custom Views/Views/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  PR-App
+//
+//  Created by Jakub Homik on 23/07/2020.
+//  Copyright © 2020 Jakub Homik. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
